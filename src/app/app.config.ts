f669e9b453df50541b64cb2f63fc90211f465a98
test <test@tests-MacBook-Pro.local>
2025-10-02 12:07:20 +0530

    adding necessary imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,30 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import { provideFirebaseApp, initializeApp} from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
 
 import { routes } from './app.routes';
+import { environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore({
+      router: routerReducer
+    }),
+    provideEffects([]),
+    provideStoreDevtools(),
+    provideRouterStore(),
+    provideFirebaseApp(()=>initializeApp(environment.firebase)),
+    provideAuth(()=>getAuth()),
+    provideFirestore(()=> getFirestore()),
   ]
 };
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment ={
+    production:false,
+    firebase:{
+        apiKey: "AIzaSyBVRJvfBRFh0sSAoBWW0muUFZvw3ViLQQk",
+  authDomain: "ngrx-jira-clone-app-42688.firebaseapp.com",
+  projectId: "ngrx-jira-clone-app-42688",
+  storageBucket: "ngrx-jira-clone-app-42688.firebasestorage.app",
+  messagingSenderId: "541113686218",
+  appId: "1:541113686218:web:c30e79633696dc8a683c33",
+  measurementId: "G-DS7K6H2FCJ"
+
+    }
+}
